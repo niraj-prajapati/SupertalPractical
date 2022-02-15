6d98546d74d06112bdca4f8644e2050c8568df65
Niraj Prajapati <niraj06081995@gmail.com>
2022-02-15 18:27:48 +0530

    change base url as mockapi.io is offline for maintenance from long time

--- app/src/main/java/com/supertal/practical/network/APIs.kt
+++ app/src/main/java/com/supertal/practical/network/APIs.kt
@@ -2,7 +2,7 @@ package com.supertal.practical.network
 
 class APIs {
     companion object {
-        const val BASE_URL = "https://6204ec80161670001741b063.mockapi.io/"
+        const val BASE_URL = "https://supertal.free.beeceptor.com/"
 
         const val GET_USERS = "users"
     }
